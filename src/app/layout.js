9b43c4437bbeec825a622a19bd65d8084a578b6d
Ian M Knott <iknott@mail.sfsu.edu>
2025-05-20 18:21:07 -0400

    update to layout, auth and package.json.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,22 @@
+import ClientOnlyLayout from '@/components/ClientOnlyLayout';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
-import { Toaster } from 'react-hot-toast';
-import "@/app/globals.css";
+import '@/app/globals.css';
 
 export default function LocaleLayout({ children }) {
-
   return (
-    <html lang='en' className="dark">
+    <html lang="en" className="dark">
       <script
         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_PLACES_KEY}&libraries=places`}
         async
         defer
       ></script>
-
       <body className="bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-white">
-        <Navbar />
-        <main className="pt-10 px-4 min-h-screen">
-          {children}
-        </main>
-        <Footer />
+        <ClientOnlyLayout>
+          <Navbar />
+          <main className="pt-10 px-4 min-h-screen">{children}</main>
+          <Footer />
+        </ClientOnlyLayout>
       </body>
     </html>
   );
