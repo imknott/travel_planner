21c9fbfc6840c388f31226f4627ca035875accf7
Ian M Knott <iknott@mail.sfsu.edu>
2025-05-25 18:38:36 -0400

    update to login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,9 @@
-'use client'
-
 import dynamic from 'next/dynamic';
 
 const LoginClient = dynamic(() => import('@/components/LoginClient'), {
   ssr: false,
 });
 
-export default function LoginPageWrapper() {
+export default function LoginPage() {
   return <LoginClient />;
 }
